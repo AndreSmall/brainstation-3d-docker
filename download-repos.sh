3302cd45f50e83ac13975e12084f41a9430d6958
Andre <asmall@brainstation.io>
2019-05-23 14:27:01 -0400

    Updated shell script to download all team repos

--- download-repos.sh
+++ download-repos.sh
@@ -1,7 +1,7 @@
-BASE_SSH_REPO=git@github.com:andresmall-educator;
-REPO=( "brainstation-3d-team-master" "brainstation-3d-team-master2")
+BASE_SSH_REPO=git@github.com:andresmall-educator/brainstation-3d-team-spring-2019-team-;
+REPO=( "1" "2" "3" "4" "5" "6" "7")
 
 for repo in "${REPO[@]}"
 do
-   git clone ${BASE_SSH_REPO}/${repo}.git ./repos/${repo}
+   git clone ${BASE_SSH_REPO}${repo}.git ./repos/team-${repo}
 done
