3e7659e8ae09b3544865df232a6268d494ff3fb3
Andre <asmall@brainstation.io>
2019-05-24 14:09:38 -0400

    Updated pull, npm install script

--- pull-and-install-repos.sh
+++ pull-and-install-repos.sh
@@ -3,7 +3,10 @@ REPO=( "1" "2" "3" "4" "5" "6" "7")
 
 for repo in "${REPO[@]}"
 do
-   cd ./repos/team-${repo}
-   git pull
-   npm run install:all
+   DIR=$(cd ./repos/team-${repo} && pwd);
+   echo ''
+   echo '####################'
+   echo $DIR
+   echo '####################'
+   $(cd ${DIR} && npm run install:all)
 done
